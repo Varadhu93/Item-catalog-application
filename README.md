5335c1eb288fe8e06e0c9dd8ca2c80e602d904ac
Varadharam Ramanathan <varadhu93amrita@gmail.com>
2019-06-19 02:38:56 +0530

    Updated based on review comments

--- README.md
+++ README.md
@@ -1,18 +1,18 @@
-># Item Catalog Project
+# Item Catalog Project
 
->## About the Project
+## About the Project
 
 You will develop an application that provides a list of items within a variety of categories as well as provide a user registration and authentication system. Registered users will have the ability to post, edit and delete their own items.
 
->## Why This Project?
+## Why This Project?
 
 Modern web applications perform a variety of functions and provide amazing features and utilities to their users; but deep down, it’s really all just creating, reading, updating and deleting data. In this project, you’ll combine your knowledge of building dynamic websites with persistent data storage to create a web application that provides a compelling service to your users.
 
->## What Will I Learn?
+## What Will I Learn?
 
 You will learn how to develop a RESTful web application using the Python framework Flask along with implementing third-party OAuth authentication. You will then learn when to properly use the various HTTP methods available to you and how these methods relate to CRUD (create, read, update and delete) operations.
 
->### Pre-requisites
+## Pre-requisites
 
 * [Python2 or Python3](https://www.python.org/)
 * [vagrant](https://www.vagrantup.com/)
@@ -20,7 +20,7 @@ You will learn how to develop a RESTful web application using the Python framewo
 * [Bootstrap](https://getbootstrap.com/docs/4.3/getting-started/introduction/)
 * [Jinja2](http://jinja.pocoo.org/)
 
->### Tech Stack
+## Tech Stack
 
 * Python
 * HTML
@@ -29,13 +29,15 @@ You will learn how to develop a RESTful web application using the Python framewo
 * Flask
 * Jinja2
 
->### Requirements
+## Requirements
 
 * Clone the fullstack-nanodegree-vm
 * Launch the Vagrant VM (vagrant up)
 * Install Vagrant and VirtualBox
 
->### Bringing the VM up
+## Quickstart
+
+### Bringing the VM up
 
 Bring up the VM with the following command:
 `vagrant up`
@@ -50,16 +52,19 @@ Once inside the VM, navigate to the tournament directory with this command:
 
 * Write your Flask application locally in the vagrant/catalog directory (which will automatically be synced to /vagrant/catalog within the VM).
 
->## Running the application
+### Running the application
 
-* Run your application within the VM (python /vagrant/catalog/application.py)
-`$ python application.py`
-* Access and test your application by visiting http://localhost:8000 or http://localhost:8000/catalog
-* JSON endpoints - http://localhost:8000/catalog/JSON and for catalog items by http://localhost:8000/catalog/{catalog_id}/{item_id}/JSON
+* Run your application within the VM (python3 /vagrant/catalog/application.py)
+`$ python3 application.py` or `python application.py`
+* Access and test your application by visiting `http://localhost:8000` or `http://localhost:8000/catalog`
+* JSON endpoints:
+  1. Catalog JSON - `http://localhost:8000/catalog/JSON` 
+  2. Catalog Items JSON - `http://localhost:8000/catalog/{catalog_id}/JSON`
+  2. Item JSON - `http://localhost:8000/catalog/{catalog_id}/{item_id}/JSON` and 
 * static - css, js files etc
 * templates - path to html files
 
->### Shutting the VM down
+### Shutting the VM down
 
 When you are finished with the VM, press Ctrl-D to log out of it and shut it down with this command:
 
--- application.py
+++ application.py
@@ -15,6 +15,7 @@ from sqlalchemy.orm import sessionmaker
 from database_setup import Base, User, Catalog, Item
 from flask import session as login_session
 
+import logging
 import random
 import string
 from oauth2client.client import flow_from_clientsecrets
@@ -24,9 +25,12 @@ import json
 from flask import make_response
 import requests
 
+
 app = Flask(__name__)
 
-# Connect to itemcatalog database and create database session
+"""
+Connecting to Item catalog database
+"""
 engine = create_engine('sqlite:///itemcatalog.db',
                        connect_args={'check_same_thread': False})
 Base.metadata.bind = engine
@@ -34,21 +38,43 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
-# Client id from authentication
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+# create a file handler
+handler = logging.FileHandler('itemcatalog.log')
+handler.setLevel(logging.INFO)
+
+# create a logging format
+formatter = logging.Formatter('''%(asctime)s - %(name)s - 
+                              %(levelname)s - %(message)s''')
+handler.setFormatter(formatter)
+
+# add the handlers to the logger
+logger.addHandler(handler)
+
+"""
+Client Id Authentication
+Reading the JSON values from client_secrets.json
+"""
 CLIENT_ID = json.loads(
     open('client_secrets.json', 'r').read())['web']['client_id']
 APPLICATION_NAME = "Item Catalog Application"
 
 
-# FB connect authentication
 @app.route('/fbconnect', methods=['POST'])
 def fbconnect():
+    """
+    Logic to FB Authentication
+    Reading JSON values from fb_client_secrets.json
+    """
     if request.args.get('state') != login_session['state']:
         response = make_response(json.dumps('Invalid state parameter.'), 401)
         response.headers['Content-Type'] = 'application/json'
         return response
     access_token = request.data
 
+    logger.info("Inside fbconnect(), Open and read fb_client_secrets.json")
     app_id = json.loads(open('fb_client_secrets.json', 'r').read())[
         'web']['app_id']
     app_secret = json.loads(
@@ -56,9 +82,9 @@ def fbconnect():
     url = '''https://graph.facebook.com/oauth/access_token?grant_type=
     fb_exchange_token&client_id=%s&client_secret=%s&fb_exchange_token=%s''' % (
         app_id, app_secret, access_token)
-    h = httplib2.Http()
-    result = h.request(url, 'GET')[1]
 
+    h = httplib2.Http()
+    result = h.request(url.encode(), 'GET')[1]
     # Use token to get user info from API
     userinfo_url = "https://graph.facebook.com/v2.8/me"
     token = result.split(',')[0].split(':')[1].replace('"', '')
@@ -68,6 +94,7 @@ def fbconnect():
     h = httplib2.Http()
     result = h.request(url, 'GET')[1]
     data = json.loads(result)
+    logger.info("Setting json values to login_session")
     login_session['provider'] = 'facebook'
     login_session['username'] = data["name"]
     login_session['email'] = data["email"]
@@ -86,6 +113,7 @@ def fbconnect():
     login_session['picture'] = data["data"]["url"]
 
     # see if user exists
+    logger.info("After FB authenticating, Check if user already exists in DB")
     user_id = getUserID(login_session['email'])
     if not user_id:
         user_id = createUser(login_session)
@@ -95,37 +123,47 @@ def fbconnect():
     return output
 
 
-# FB Disconnect session
 @app.route('/fbdisconnect')
 def fbdisconnect():
+    """
+    Logic to disconnect FB session
+    """
     facebook_id = login_session['facebook_id']
     access_token = login_session['access_token']
     url = 'https://graph.facebook.com/%s/permissions?access_token=%s' % (
         facebook_id, access_token)
     h = httplib2.Http()
     result = h.request(url, 'DELETE')[1]
+    logger.info("fbdisconnect(), Logged out")
     return "you have been logged out"
 
 
-# Login to Catalog
 @app.route('/login')
 def showLogin():
+    """
+    Logic to re-direct to Login page
+    """
     state = ''.join(random.choice(string.ascii_uppercase + string.digits)
-                    for x in xrange(32))
+                    for x in range(32))
     # Add to client session the state string in order to preven anti-forgery
     login_session['state'] = state
+    logger.info("Display login page")
     return render_template('login.html', currentPage='login', STATE=state)
 
 
-# Disconnect from FB or Google and clear the session
 @app.route('/disconnect')
 def disconnect():
+    """
+    Logic to disconnect and clear
+    a User's session
+    """
     if 'provider' in login_session:
         if login_session['provider'] == 'google':
             gdisconnect()
         if login_session['provider'] == 'facebook':
             fbdisconnect()
         login_session.clear()
+        logger.info("Inside disconnect(), Clear user session and logging out")
         flash("You have successfully been logged out.")
         return redirect(url_for('showCatalogApp'))
     else:
@@ -133,9 +171,12 @@ def disconnect():
         return redirect(url_for('showCatalogApp'))
 
 
-# Google authentication
 @app.route('/gconnect', methods=['POST'])
 def gconnect():
+    """
+    Google Authentication
+    Reads data from client_secrets.json
+    """
     # Validate state token
     if request.args.get('state') != login_session['state']:
         response = make_response(json.dumps('Invalid state parameter.'), 401)
@@ -145,6 +186,7 @@ def gconnect():
     code = request.data
     try:
         # Upgrade the authorization code into a credentials object
+        logger.info("Inside gconnect(), Open and read client_secrets.json")
         oauth_flow = flow_from_clientsecrets('client_secrets.json', scope='')
         oauth_flow.redirect_uri = 'postmessage'
         credentials = oauth_flow.step2_exchange(code)
@@ -159,7 +201,7 @@ def gconnect():
     url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s'
            % access_token)
     h = httplib2.Http()
-    result = json.loads(h.request(url, 'GET')[1])
+    result = json.loads(h.request(url, 'GET')[1].decode())
     # If there was an error in the access token info, abort.
     if result.get('error') is not None:
         response = make_response(json.dumps(result.get('error')), 500)
@@ -208,22 +250,23 @@ def gconnect():
     login_session['email'] = data['email']
     # ADD PROVIDER TO LOGIN SESSION
     login_session['provider'] = 'google'
-    print(login_session)
     # see if user exists, if it doesn't make a new one
     user_id = getUserID(data["email"])
     if not user_id:
         user_id = createUser(login_session)
     login_session['user_id'] = user_id
-
     output = "<h6><strong>Redirecting...</strong></h6>"
     flash("Welcome {} you are now logged in".format(login_session['username']))
     return output
 
 
-# Google Disconnect
 @app.route('/gdisconnect')
 def gdisconnect():
+    """
+    Disconnect User's Google session
+    """
     # Only disconnect a connected user.
+    logger.info("Inside gdisconnect(), Disconnect google connect session")
     access_token = login_session.get('access_token')
     if access_token is None:
         response = make_response(
@@ -244,8 +287,10 @@ def gdisconnect():
         return response
 
 
-# Add new user to User Table in DB
 def createUser(login_session):
+    """
+    Adding a new User into User table
+    """
     try:
         if('username' not in login_session):
             login_session['username'] = "Admin_User"
@@ -255,6 +300,7 @@ def createUser(login_session):
         session.commit()
         user = session.query(User).filter_by(
             email=login_session['email']).one()
+        logger.info("Added new user into User Table %s " % user.id)
         return user.id
     except:
         session.rollback()
@@ -262,42 +308,58 @@ def createUser(login_session):
         session.close()
 
 
-# Get user info from DB
 def getUserInfo(user_id):
+    """
+    Fetch User data or object using User Id
+    """
+    logger.info("Inside getUserInfo(), Get user data using id %s " % user_id)
     user = session.query(User).filter_by(id=user_id).one()
     return user
 
 
-# Get user ID
 def getUserID(email):
+    """
+    Fetch User id using User email id
+    """
     try:
+        logger.info("Inside getUserID(), Get user id for username %s " % email)
         user = session.query(User).filter_by(email=email).one()
         return user.id
     except:
         return None
 
 
-# Show all categories in item catalog app
 @app.route('/')
 @app.route('/catalog/')
 def showCatalogApp():
+    """
+    Home page to Catalog Application
+    Displays all the Categories and Items
+    """
     catalogs = session.query(Catalog).all()
     items = session.query(Item, Catalog).join(Catalog).filter(
         Catalog.id == Item.catalog_id).order_by(Item.date.desc()).limit(10)
+    logger.info("Inside showCatalogApp(), Return catalog.html - Home page")
     return render_template('catalog.html', catalogs=catalogs, items=items,
                            currentPage='catalog')
 
 
-# Returns JSON value for catalog app
-@app.route('/catalog/json')
+@app.route('/catalog/JSON')
 def showCatalogJSON():
+    """
+    Returns the JSON values for the Categories
+    """
     catalogs = session.query(Catalog).all()
-    return jsonify(restaurants=[c.serialize for c in catalogs])
+    logger.info("""Inside showCatalogJSON(), Returns JSON
+                values for all categories""")
+    return jsonify(catalogs=[c.serialize for c in catalogs])
 
 
-# Adding a new Catalog to the Item catalog application
 @app.route('/catalog/new/', methods=['GET', 'POST'])
 def newCatalog():
+    """
+    Adds a new Category to the Catalog application
+    """
     # First check the user state, if key <username> is in session
     if 'username' not in login_session:
         flash("To create one category you must be logged in")
@@ -310,6 +372,7 @@ def newCatalog():
             session.add(newCatalog)
             session.commit()
             flash("{} successfully added to Catalogs".format(newCatalog.name))
+            logger.info("Added new catalog")
             return redirect(url_for('showCatalogApp'))
         else:
             flash("Cannot create an empty category")
@@ -318,9 +381,11 @@ def newCatalog():
         return render_template('newcatalog.html', currentPage='newcatalog')
 
 
-# Editing a catalog
 @app.route('/catalog/<int:catalog_id>/edit/', methods=['GET', 'POST'])
 def editCatalog(catalog_id):
+    """
+    To edit/modify an exiting Category in Catalog
+    """
     # To edit one category user must be logged in & be the category creator
     if 'username' not in login_session:
         flash("To edit a category you must be logged in")
@@ -335,6 +400,7 @@ def editCatalog(catalog_id):
             session.commit()
             flash("Catalog {} successfully edited to {}".format(editedCatalog,
                                                                 catalog.name))
+            logger.info("Edited existing catalog %s " % catalog.name)
             return redirect(url_for('showCatalogApp'))
         else:
             # If user doesn't edit specific category msg below is displayed
@@ -346,19 +412,19 @@ def editCatalog(catalog_id):
                                currentPage='editcatalog')
 
 
-# Delete a catalog
 @app.route('/catalog/<int:catalog_id>/delete/', methods=['GET', 'POST'])
 def deleteCatalog(catalog_id):
+    """
+    To delete an existing Category from the Catalog
+    """
     # To delete one category user must be logged in & be the category creator
     if 'username' not in login_session:
         flash("To delete a category you must be logged in")
         return redirect('/login')
     else:
         catalog = session.query(Catalog).filter_by(id=catalog_id).one()
-        print(catalog.user_id)
         deletedCatalog = catalog.name
     if catalog.user_id != login_session['user_id']:
-        print("User Id %s" % catalog.user_id)
         flash("You a not authorized to delete {} category".format(
             deletedCatalog))
         return redirect(url_for('showCatalogItems', catalog_id=catalog.id))
@@ -366,43 +432,66 @@ def deleteCatalog(catalog_id):
         session.delete(catalog)
         session.commit()
         flash("{} successfully deleted from Catalog".format(deletedCatalog))
+        logger.info("Deleting a catalog %s " % deletedCatalog)
         return redirect(url_for('showCatalogApp'))
     else:
         return render_template('deletecatalog.html', catalog=catalog,
                                currentPage='deletecatalog')
 
 
-# Show Catalog items
 @app.route('/catalog/<int:catalog_id>/items/')
 def showCatalogItems(catalog_id):
+    """
+    Display all the items under a specific Category
+    """
     catalogs = session.query(Catalog).all()
     catalog = session.query(Catalog).filter_by(id=catalog_id).one()
     items_catalog = session.query(Item).filter_by(catalog_id=catalog_id).all()
+    logger.info("Inside showCatalogItems, Show all items inside catalog")
     return render_template('catalogitem.html', catalogs=catalogs,
                            catalog=catalog, items=items_catalog,
                            currentPage='catalogitem')
 
 
-# Returns a Catalog Items JSON value
 @app.route('/catalog/<int:catalog_id>/items/JSON')
 def catalogItemsJSON(catalog_id):
+    """
+    returns the JSON values for all items under a Category
+    """
     catalogItems = session.query(Item).filter_by(catalog_id=catalog_id).all()
+    logger.info("JSON values for a catalog and it's items")
     return jsonify(catalogItems=[c.serialize for c in catalogItems])
 
 
-# Choosing an item in category returns the information of the item
+@app.route('/catalog/<int:catalog_id>/<int:item_id>/JSON')
+def itemJSON(catalog_id, item_id):
+    """
+    Implementation of JSON endpoint for an
+    item present inside a category
+    """
+    item = session.query(Item).filter_by(id=item_id).one()
+    logger.info("JSON values for a specific item")
+    return jsonify(item=[item.serialize])
+
+
 @app.route('/catalog/<int:catalog_id>/<int:item_id>/')
 def showCatalogItemInfo(catalog_id, item_id):
+    """
+    Returns the description of an item chosen
+    """
     catalog = session.query(Catalog).filter_by(id=catalog_id).one()
     item = session.query(Item).filter_by(id=item_id).one()
+    logger.info("Displaying catalog item description")
     return render_template('catalogiteminfo.html', catalog=catalog,
                            item=item, currentPage='catalogiteminfo')
 
 
-# Editing an item inside category
 @app.route('/catalog/<int:catalog_id>/items/<int:item_id>/edit/',
            methods=['GET', 'POST'])
 def editCatalogItem(catalog_id, item_id):
+    """
+    Edit/modify a category item
+    """
     if 'username' not in login_session:
         flash("To edit an item you must be logged in")
         return redirect('/login')
@@ -424,21 +513,23 @@ def editCatalogItem(catalog_id, item_id):
             session.add(item)
             session.commit()
             flash("{} successfully edited".format(editedItem))
+            logger.info("Editing the catalog item %s " % editedItem)
             return redirect(url_for('showCatalogItems', catalog_id=catalog.id))
         else:
             flash("Nothing Changed")
             return redirect(url_for('showCatalogItems', catalog_id=catalog.id))
     else:
-        print('edit catalog page')
         return render_template('editcatalogitem.html', catalogs=catalogs,
                                catalog=catalog, item=item,
                                currentPage='editcatalogitem')
 
 
-# Deleting an item from a category
 @app.route('/catalog/<int:catalog_id>/items/<int:item_id>/delete/',
            methods=['GET', 'POST'])
 def deleteCatalogItem(catalog_id, item_id):
+    """
+    Deletes an item from the Category
+    """
     if 'username' not in login_session:
         flash("To delete an item you must be logged in")
         return redirect('/login')
@@ -457,15 +548,18 @@ def deleteCatalogItem(catalog_id, item_id):
         session.delete(itemToDelete)
         session.commit()
         flash("{} successfully deleted".format(deletedItem))
+        logger.info("Deleting a catalog item %s " % deletedItem)
         return redirect(url_for('showCatalogItems', catalog_id=catalog.id))
     else:
         return render_template('catalogiteminfo.html', catalog=catalog,
                                item=itemToDelete)
 
 
-# Add new item in a category
 @app.route('/catalog/item/new/', methods=['GET', 'POST'])
 def newItem():
+    """
+    Adds a new item under a Category
+    """
     if 'username' not in login_session:
         flash("To create a new item you must be logged in")
         return redirect('/login')
@@ -486,6 +580,7 @@ def newItem():
             session.commit()
             flash("{} successfully added to {}".format(newItem.title,
                                                        catalog.name))
+            logger.info("Added new item to catalog %s " % newItem.title)                                           
             return redirect(url_for('showCatalogItems', catalog_id=catalog.id))
         else:
             flash("Couldn't create new Catalog Item")
@@ -497,5 +592,9 @@ def newItem():
 
 
 if __name__ == '__main__':
+    """
+    Application execution point and port configuration
+    """
+    logger.info("ITEM CATALOG APPLICATION LOGS")
     app.secret_key = 'super_secret_key'
     app.run(debug=True, use_reloader=False, host='0.0.0.0', port=8000)
